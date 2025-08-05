edec73ba0999c0d7845b1bc997f6e67b2b3f2838
ainumismatic <194127868+ainumismatic@users.noreply.github.com>
2025-08-05 20:59:07 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 import uuid
 from datetime import datetime, timedelta
-from emergentintegrations.llm.chat import LlmChat, UserMessage
+##from emergentintegrations.llm.chat import LlmChat, UserMessage
 import json
 import re
 
